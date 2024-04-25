23fc3bf555a99a9ed88a378c4ebada1c5acd3737
chap-ssah <cmaeresera1@gmail.com>
2024-04-25 16:39:37 +0200

    implement Forms

---
+++ app/form/SignInForm.tsx
@@ -0,0 +1,87 @@
+'use client'
+import { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Input } from '../ui/input';
+import { Button } from '../ui/button';
+import Link from 'next/link';
+import { myContext } from '../../app/Main';
+import { useRouter } from 'next/navigation';
+
+const FormSchema = z.object({
+  email: z.string().min(1, 'Email is required').email('Invalid email'),
+  password: z.string().min(1, 'Password is required').min(4, 'Password must have more than 4 characters')
+});
+
+const ModalForm = () => {
+ 
+  const { register, handleSubmit, formState: { errors } } = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema)
+  });
+
+  const {closeModal, isModalOpen} = useContext(myContext)
+
+  const router = useRouter()
+
+  const onSubmit = (values: z.infer<typeof FormSchema>) => {
+    console.log(values);
+    closeModal();
+  };
+
+
+
+  return (
+    <>
+
+      {isModalOpen && (
+        <div className="modal-overlay">
+          <div className="modal-content">
+            <form onSubmit={handleSubmit(onSubmit)} className="w-full">
+              <div className="space-y-2">
+                <div className="form-field">
+                  <label htmlFor="email" className="form-label">
+                    Email
+                  </label>
+                  <div className="form-control">
+                    <Input id="email" placeholder="mail@example.com" {...register('email')} />
+                  </div>
+                  {errors.email && <div className="form-message">{errors.email.message}</div>}
+                </div>
+
+                <div className="form-field">
+                  <label htmlFor="password" className="form-label">
+                    Password
+                  </label>
+                  <div className="form-control">
+                    <Input id="password" type="password" placeholder="Enter password" {...register('password')} />
+                  </div>
+                  {errors.password && <div className="form-message">{errors.password.message}</div>}
+                </div>
+              </div>
+
+              <Button className="w-full mt-6 bg-pink-600" type="submit">
+                Sign in
+              </Button>
+            </form>
+
+            <p className="text-center text-sm text-white mt-2">
+              If you don&apos;t have an account, please&nbsp;
+              <Link className="text-pink-600 hover:underline" href="/signUp">
+                Sign up
+              </Link>
+            </p>
+            <button className="modal-close" onClick={()=> {
+              closeModal()
+              router.push('./')
+            }}>
+              Close
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ModalForm;
